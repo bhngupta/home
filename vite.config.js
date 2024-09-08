0d64faba3d459e7ff0b68c539febba55bc9f2b30
Bhanu Gupta <bhanu.mbvg@gmail.com>
2024-09-08 05:40:07 -0400

    fix: deploy to coluflare

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/home/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 3000,
