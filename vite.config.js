67f75c4d01e775753148dfb5f0595c6e94c38b18
Bhanu Gupta <bhanu.mbvg@gmail.com>
2024-09-08 04:29:08 -0400

    fix: deploy code

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/home/',
   plugins: [react()],
   server: {
     port: 3000,
   },
-})
+});
